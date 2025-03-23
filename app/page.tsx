6bf87f19cb923d7cf389c3ef7cc7e37f7ced5416
Sandro <sandro.bengoa@gmail.com>
2025-03-23 15:44:33 -0500

    take out contact

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <SectionDivider hideDivider />
       <Projects />
       <SectionDivider hideDivider />
-      <Contact />
+      {/* <Contact /> */}
     </main>
   );
 }
